8fde508ea2c43a88222fc5c55d06dd401caf9bcc
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-09-20 11:56:41 -0700

    build-framework  -  Added a number of tests

--- spec/agent42.spec.sh
+++ spec/agent42.spec.sh
@@ -14,4 +14,24 @@ test $? -eq 0 && echo "✅ pass feature "$VERSION "is a feature version" || echo
 
 VERSION="1.40.0"
 ./bin/agent42 feature $VERSION  > /dev/null
-test $? -eq 1 && echo "✅ pass feature "$VERSION "is not a feature version"|| echo "🚫 fail feature "$VERSION "is a feature version"
+test $? -eq 1 && echo "✅ pass feature "$VERSION "is not a feature version" || echo "🚫 fail feature "$VERSION "is a feature version"
+
+PULL_REQUEST=false
+VERSION=1.40.0
+./bin/agent42 tag $PULL_REQUEST $VERSION  > /dev/null
+test $? -eq 0 && echo "✅ pass tag "$PULL_REQUEST "is not a pull request build" || echo "🚫 fail tag "$PULL_REQUEST "is a pull request build"
+
+PULL_REQUEST=42
+VERSION=0.0.2
+./bin/agent42 tag $PULL_REQUEST $VERSION  > /dev/null
+test $? -eq 1 && echo "✅ pass tag "$VERSION "does not match CHANGELOG" || echo "🚫 fail tag "$VERSION "matches CHANGELOG"
+
+PULL_REQUEST=42
+VERSION=0.0.1
+./bin/agent42 tag $PULL_REQUEST $VERSION  > /dev/null
+test $? -eq 3 && echo "✅ pass tag v"$VERSION "already exists for version" || echo "🚫 fail tag v"$VERSION "does not exists for version"
+
+# PULL_REQUEST=1
+# VERSION="0.0.1"
+# ./bin/agent42 tag $PULL_REQUEST $VERSION  > /dev/null
+# test $? -eq 3 && echo "✅ pass tag "$VERSION "does not match CHANGELOG" || echo "🚫 fail tag "$VERSION "matches CHANGELOG"
